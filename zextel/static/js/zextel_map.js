3372a1aa0567c36dd191150717c290b0fafdcebf
Slimexpert <59517032+slimexpert@users.noreply.github.com>
2020-03-20 01:17:13 +0300

    1

--- zextel/static/js/zextel_map.js
+++ zextel/static/js/zextel_map.js
@@ -1,16 +1,17 @@
 ymaps.ready(function () {
 
     var map = new ymaps.Map('map', {
-            center: [60.027346, 30.661338],
+            center: [55.79, 37.64],
             zoom: 10,
             controls: ['zoomControl']
         }),
         objectManager = new ymaps.ObjectManager();
 
-    map.controls.get('zoomControl').options.set({size: 'small'});
+    //map.controls.get('zoomControl').options.set({size: 'small'});
 
     // Загружаем GeoJSON файл, экспортированный из Конструктора карт.
-    $.getJSON('https://slimexpert.github.io/zextel/static/js/main.geojson').done(function (geoJson) 
+    $.getJSON('https://slimexpert.github.io/zextel/static/js/main1.geojson')
+    	.done(function (geoJson) 
     	{
            // Добавляем описание объектов в формате JSON в менеджер объектов.
         	objectManager.add(geoJson);
